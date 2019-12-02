8e5b3f160b40b012f8bb3a63bdb2ba80ea291309
Evan Wappel <ewappel@gmail.com>
2019-12-02 15:08:58 -0800

    Update colab.py

--- colab.py
+++ colab.py
@@ -27,4 +27,4 @@ from google.colab import auth
 auth.authenticate_user()
 from oauth2client.client import GoogleCredentials
 gc = gspread.authorize(GoogleCredentials.get_application_default())
-%load_ext google.colab.data_table
+
